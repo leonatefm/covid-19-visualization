e2898aee70927e6c1c83e3bdd4fa72f45daef5d6
Jing <Jing0713@users.noreply.github.com>
2020-04-06 21:01:29 -0700

    formatted world_aggregated data

--- src/data/main.py
+++ src/data/main.py
@@ -2,21 +2,10 @@ import csv
 import requests
 import json
 
-csv_url = 'https://pkgstore.datahub.io/core/covid-19/worldwide-aggregated_csv/data/8c5dc6c56dcbe2b467d1527a38fd0eeb/worldwide-aggregated_csv.csv'
-
-with requests.Session() as s:
-    download = s.get(csv_url)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-
-    my_list = list(cr)
-    # for row in my_list:
-    #     print(row)
-    # print(my_list)
-
 def write_json(filepath, data):
     with open(filepath, 'w', newline='', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
+
 
 
 def file_path(filename):
@@ -24,9 +13,32 @@ def file_path(filename):
     return new_filepath
 
 
+
 def main():
+
+    csv_url = 'https://pkgstore.datahub.io/core/covid-19/worldwide-aggregated_csv/data/8c5dc6c56dcbe2b467d1527a38fd0eeb/worldwide-aggregated_csv.csv'
+
+    with requests.Session() as s:
+        download = s.get(csv_url)
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+
+    my_list = list(cr)
+    my_list[0] = ['date', 'confirmed', 'recovered', 'deaths', 'increase_rate']
+
+    data_list = []
+    for i, row in enumerate(my_list):
+        if i == 0:
+            labels = row
+        else:
+            each_day_data = {}
+            for j, value in enumerate(row):
+                each_day_data[labels[j]] = value
+            data_list.append(each_day_data)
+
+
     filename = 'world_aggregated'
-    write_json(file_path(filename), my_list)
+    write_json(file_path(filename), data_list)
 
 
 
